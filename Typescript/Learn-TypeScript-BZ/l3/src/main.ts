7a23efce79549d9c4998caf466f7111ddaac6db1
Luka-DV <luka.guva@gmail.com>
2024-10-31 17:06:37 +0100

    add function return types

--- Typescript/Learn-TypeScript-BZ/l3/src/main.ts
+++ Typescript/Learn-TypeScript-BZ/l3/src/main.ts
@@ -22,11 +22,11 @@ let cashInRegister = 100
 let nextOrderId = 1
 const orderQueue: Order[] = []
 
-function addNewPizza(pizzaObj: Pizza) {
+function addNewPizza(pizzaObj: Pizza): void {
     menu.push(pizzaObj)
 }
 
-function placeOrder(pizzaName: string) {
+function placeOrder(pizzaName: string): Order | undefined {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
     if (!selectedPizza) {
         console.error(`${pizzaName} does not exist in the menu`)
@@ -38,7 +38,7 @@ function placeOrder(pizzaName: string) {
     return newOrder
 }
 
-function completeOrder(orderId: number) {
+function completeOrder(orderId: number): Order | undefined {
     const order = orderQueue.find(order => order.id === orderId)
     if (!order) {
         console.error(`${orderId} was not found in the orderQueue`)
@@ -49,7 +49,7 @@ function completeOrder(orderId: number) {
 }
 
 
-function getPizzaDetail(identifier: string | number) {
+function getPizzaDetail(identifier: string | number): Pizza | undefined {
     
     const pizzaDetail = menu.find( pizzaObj => typeof identifier === "string" ?
         pizzaObj.name.toLowerCase() === identifier.toLowerCase() : 
@@ -61,6 +61,19 @@ function getPizzaDetail(identifier: string | number) {
      write the code to check if the parameter is a string
      * or a number, and use the menu.find() method accordingly
      */
+
+
+    //its good to be explicit whenever you can, if someone is using the js file:
+
+    //  function getPizzaDetail(identifier: string | number) {
+    //     if (typeof identifier === "string") {
+    //         return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
+    //     } else if (typeof identifier === "number") {
+    //         return menu.find(pizza => pizza.id === identifier)
+    //     } else {
+    //         throw new TypeError("Parameter `identifier` must be either a string or a number")
+    //     }
+    // }
 }
 
 addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
