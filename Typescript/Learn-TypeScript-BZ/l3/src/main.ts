b4385c1bab7ab1980cc9e257b96b1cc223fbcb70
Luka-DV <luka.guva@gmail.com>
2024-11-01 13:42:26 +0100

    add automatic ids to menu items

--- Typescript/Learn-TypeScript-BZ/l3/src/main.ts
+++ Typescript/Learn-TypeScript-BZ/l3/src/main.ts
@@ -11,18 +11,21 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
-]
-
+let nextPizzaId = 1
 let cashInRegister = 100
 let nextOrderId = 1
 const orderQueue: Order[] = []
 
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: "Margherita", price: 8 },
+    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaId++, name: "Veggie", price: 9 },
+]
+
+
 function addNewPizza(pizzaObj: Pizza): void {
+    pizzaObj.id = nextPizzaId++;
     menu.push(pizzaObj)
 }
 
@@ -76,17 +79,17 @@ function getPizzaDetail(identifier: string | number): Pizza | undefined {
     // }
 }
 
-addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
-placeOrder("Chicken Bacon Ranch")
+/* placeOrder("Chicken Bacon Ranch")
 placeOrder("Pepperoni")
 completeOrder(1)
 placeOrder("Anchovy")
 placeOrder("Veggie")
 completeOrder(2)
-
+ */
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
 console.log("Order queue:", orderQueue)
